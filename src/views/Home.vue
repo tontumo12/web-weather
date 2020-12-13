55adf34891ae1813b349a59d189016d1dcc86031
thuhoai1810 <54019657+thuhoai1810@users.noreply.github.com>
2020-12-13 18:25:27 +0700

    add form

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -47,6 +47,9 @@
           <v-btn depressed color="primary" @click="hour = true">
             Xem theo giờ
           </v-btn>
+          <v-btn depressed color="primary" @click="day = true">
+            Xem theo ngày
+          </v-btn>
         </v-col>
         <!-- hiệu ứng thời tiết -->
         <v-col cols="6">
@@ -120,14 +123,29 @@
     <v-card v-if="hour == true">
       <v-data-table
         dense
-        :headers="headers"
-        :items="desserts"
+        :headers="headers1"
+        :items="desserts1"
         item-key="time"
         class="elevation-1"
       >
         <template v-slot:top>
           <v-spacer></v-spacer>
           <v-btn color="primary" dark class="mb-2" @click="hour = false">
+            close
+          </v-btn>
+        </template>
+      </v-data-table>
+    </v-card>
+    <v-card v-if="day == true">
+      <v-data-table
+      :headers="headers"
+      :items="desserts"
+      :items-per-page="5"
+      class="elevation-1"
+      >
+        <template v-slot:top>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" dark class="mb-2" @click="day = false">
             close
           </v-btn>
         </template>
@@ -148,7 +166,8 @@ export default {
     cloud: false,
     humidity: 60,
     hour: false,
-    desserts: [
+    day: false,
+    desserts1: [
       {
         time: "1 am",
         temperature: 159,
@@ -220,7 +239,7 @@ export default {
         chance_rain: 7,
       },
     ],
-    headers: [
+    headers1: [
       {
         text: "Thời gian",
         align: "start",
@@ -232,6 +251,101 @@ export default {
       { text: "Tốc độ gió", value: "wind" },
       { text: "Khả năng mưa", value: "chance_rain" },
     ],
+    headers: [
+          {
+            text: 'Dessert (100g serving)',
+            align: 'start',
+            sortable: false,
+            value: 'name',
+          },
+          { text: 'Calories', value: 'calories' },
+          { text: 'Fat (g)', value: 'fat' },
+          { text: 'Carbs (g)', value: 'carbs' },
+          { text: 'Protein (g)', value: 'protein' },
+          { text: 'Iron (%)', value: 'iron' },
+        ],
+        desserts: [
+          {
+            name: 'Frozen Yogurt',
+            calories: 159,
+            fat: 6.0,
+            carbs: 24,
+            protein: 4.0,
+            iron: '1%',
+          },
+          {
+            name: 'Ice cream sandwich',
+            calories: 237,
+            fat: 9.0,
+            carbs: 37,
+            protein: 4.3,
+            iron: '1%',
+          },
+          {
+            name: 'Eclair',
+            calories: 262,
+            fat: 16.0,
+            carbs: 23,
+            protein: 6.0,
+            iron: '7%',
+          },
+          {
+            name: 'Cupcake',
+            calories: 305,
+            fat: 3.7,
+            carbs: 67,
+            protein: 4.3,
+            iron: '8%',
+          },
+          {
+            name: 'Gingerbread',
+            calories: 356,
+            fat: 16.0,
+            carbs: 49,
+            protein: 3.9,
+            iron: '16%',
+          },
+          {
+            name: 'Jelly bean',
+            calories: 375,
+            fat: 0.0,
+            carbs: 94,
+            protein: 0.0,
+            iron: '0%',
+          },
+          {
+            name: 'Lollipop',
+            calories: 392,
+            fat: 0.2,
+            carbs: 98,
+            protein: 0,
+            iron: '2%',
+          },
+          {
+            name: 'Honeycomb',
+            calories: 408,
+            fat: 3.2,
+            carbs: 87,
+            protein: 6.5,
+            iron: '45%',
+          },
+          {
+            name: 'Donut',
+            calories: 452,
+            fat: 25.0,
+            carbs: 51,
+            protein: 4.9,
+            iron: '22%',
+          },
+          {
+            name: 'KitKat',
+            calories: 518,
+            fat: 26.0,
+            carbs: 65,
+            protein: 7,
+            iron: '6%',
+          },
+        ],
   }),
 };
 </script>
