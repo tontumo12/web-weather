4573c6a4855cd6d7ad1f6e3a5bd5a56e1352cd08
nguyenthuy1406 <62329252+nguyenthuy1406@users.noreply.github.com>
2020-12-13 17:25:52 +0700

    add admin

---
+++ src/views/Admin.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-container>
+    <v-app-bar class="app-bar">
+      <!-- logo -->
+      <v-img
+        alt="weather logo"
+        class="shrink logo"
+        contain
+        src="../assets/weather.jpg"
+        min-width="65"
+        width="65"
+      />
+      <h1 class="display-1.5 font-weight-bold mb-3">weather</h1>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+    </v-container>
+</template>
