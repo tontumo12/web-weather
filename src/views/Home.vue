89d3410432d0b742e096f213e1dfba0bb8ced2a4
thien021998 <tieuquy16021998@gmail.com>
2020-12-13 12:43:48 +0700

    edit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <v-container class="app">
     <v-app-bar class="app-bar">
+      <!-- logo -->
       <v-img
         alt="weather logo"
         class="shrink logo"
@@ -11,6 +12,7 @@
       />
       <h1 class="display-1.5 font-weight-bold mb-3">weather</h1>
       <v-spacer></v-spacer>
+      <!-- thanh tìm kiếm -->
       <v-text-field
         label="Tìm kiếm"
         placeholder="nhập tên thành phố"
@@ -29,37 +31,81 @@
     <v-divider></v-divider>
     <v-divider></v-divider>
     <v-divider></v-divider>
+    <!-- hiển thị địa chỉ theo map -->
     <v-card flat class="text-center">
       <p><v-icon>mdi-map-marker</v-icon> tương mai, hai bà trưng, hà nội</p>
     </v-card>
     <v-card flat>
       <v-row class="text-center">
+        <!-- khung hiển thị thời tiết -->
         <v-col cols="6">
-          <h1 class="display-2 font-weight-bold mb-2">Hà nội</h1>
-          <h1>Nhiệt độ : 20</h1>
-          <h1>Độ Ẩm: 40%</h1>
-          <h1>Tốc độ gió: 20km/h</h1>
-          <h1>Khả năng mưa : 50%</h1>
-          <v-btn depressed color="primary" @click="hour = true"> Primary </v-btn>
+          <h1 class="display-2 font-weight-bold mb-2">{{ city }}</h1>
+          <h1>Nhiệt độ : {{ NhietDo }} °C</h1>
+          <h1>Độ Ẩm: {{ humidity }} %</h1>
+          <h1>Tốc độ gió: {{ wind }} km/h</h1>
+          <h1>Khả năng mưa : {{ rain }} %</h1>
+          <v-btn depressed color="primary" @click="hour = true">
+            Primary
+          </v-btn>
         </v-col>
+        <!-- hiệu ứng thời tiết -->
         <v-col cols="6">
-          <v-card class="body" style="background-color: #010a3d" >
-            <p class="rain-drop"></p>
-            <p class="rain-drop"></p>
-            <p class="rain-drop"></p>
-            <p class="rain-drop"></p>
-            <p class="rain-drop"></p>
-            <p class="rain-drop"></p>
-            <p class="rain-drop"></p>
-            <p class="rain-drop"></p>
-            <p class="rain-drop"></p>
-            <p class="rain-drop"></p>
-            <p class="rain-drop"></p>
-            <p class="rain-drop"></p>
-            <p class="rain-drop"></p>
-            <p class="rain-drop"></p>
-            <p class="rain-drop"></p>
-          </v-card>
+          <!-- hiệu ứng nắng -->
+          <!-- <v-card class="body" style="background-color: rgb(23, 188, 238)">
+            <div class="hot">
+              <span class="sun"></span>
+              <span class="sunx"></span>
+            </div>
+          </v-card> -->
+          <!-- hiệu ứng mây -->
+          <!-- <v-card class="body" style="background-color: #fff">
+            <div class="hot">
+              <span class="sun"></span>
+              <span class="sunx"></span>
+            </div>
+            <div class="cloudy">
+              <span class="cloud"></span>
+              <span class="cloudx"></span>
+            </div>
+            <div class="stormy">
+              <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+              </ul>
+              <span class="snowe"></span>
+              <span class="snowex"></span>
+              <span class="stick"></span>
+              <span class="stick2"></span>
+            </div>
+            <div class="breezy">
+              <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+              </ul>
+              <span class="cloudr"></span>
+            </div>
+            <div class="night">
+              <span class="moon"></span>
+              <span class="spot1"></span>
+              <span class="spot2"></span>
+              <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+              </ul>
+            </div>
+          </v-card> -->
         </v-col>
       </v-row>
     </v-card>
@@ -68,24 +114,19 @@
     <v-divider></v-divider>
     <v-card v-if="hour == true">
       <v-data-table
-    dense
-    :headers="headers"
-    :items="desserts"
-    item-key="time"
-    class="elevation-1"
-  >
-  <template v-slot:top>
-    <v-spacer></v-spacer>
-    <v-btn
-              color="primary"
-              dark
-              class="mb-2"
-              @click="hour = false"
-            >
-              New Item
-            </v-btn>
-  </template>
-    </v-data-table>
+        dense
+        :headers="headers"
+        :items="desserts"
+        item-key="time"
+        class="elevation-1"
+      >
+        <template v-slot:top>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" dark class="mb-2" @click="hour = false">
+            New Item
+          </v-btn>
+        </template>
+      </v-data-table>
     </v-card>
   </v-container>
 </template>
@@ -95,92 +136,98 @@
 
 export default {
   data: () => ({
+    city: "Hà nội",
+    NhietDo: 30,
+    rain: 50,
+    wind: 30,
+    cloud: false,
+    humidity: 60,
     hour: false,
-      desserts: [
-        {
-          time: '1 am',
-          temperature: 159,
-          humidity: 6.0,
-          wind: 24,
-          chance_rain: 4.0,
-        },
-        {
-          time: '2 am',
-          temperature: 237,
-          humidity: 9.0,
-          wind: 37,
-          chance_rain: 4.3,
-        },
-        {
-          time: '3 am',
-          temperature: 262,
-          humidity: 16.0,
-          wind: 23,
-          chance_rain: 6.0,
-        },
-        {
-          time: '4 am',
-          temperature: 305,
-          humidity: 3.7,
-          wind: 67,
-          chance_rain: 4.3,
-        },
-        {
-          time: '5 am',
-          temperature: 356,
-          humidity: 16.0,
-          wind: 49,
-          chance_rain: 3.9,
-        },
-        {
-          time: '6 am',
-          temperature: 375,
-          humidity: 0.0,
-          wind: 94,
-          chance_rain: 0.0,
-        },
-        {
-          time: '7 am',
-          temperature: 392,
-          humidity: 0.2,
-          wind: 98,
-          chance_rain: 0,
-        },
-        {
-          time: '8 am',
-          temperature: 408,
-          humidity: 3.2,
-          wind: 87,
-          chance_rain: 6.5,
-        },
-        {
-          time: '9 am',
-          temperature: 452,
-          humidity: 25.0,
-          wind: 51,
-          chance_rain: 4.9,
-        },
-        {
-          time: '10 am',
-          temperature: 518,
-          humidity: 26.0,
-          wind: 65,
-          chance_rain: 7,
-        },
-      ],
-      headers: [
-        {
-          text: 'Thời gian',
-          align: 'start',
-          sortable: false,
-          value: 'time',
-        },
-        { text: 'Nhiệt độ', value: 'temperature' },
-        { text: 'Độ ẩm', value: 'humidity' },
-        { text: 'Tốc độ gió', value: 'wind' },
-        { text: 'Khả năng mưa', value: 'chance_rain' },
-      ],
-    }),
+    desserts: [
+      {
+        time: "1 am",
+        temperature: 159,
+        humidity: 6.0,
+        wind: 24,
+        chance_rain: 4.0,
+      },
+      {
+        time: "2 am",
+        temperature: 237,
+        humidity: 9.0,
+        wind: 37,
+        chance_rain: 4.3,
+      },
+      {
+        time: "3 am",
+        temperature: 262,
+        humidity: 16.0,
+        wind: 23,
+        chance_rain: 6.0,
+      },
+      {
+        time: "4 am",
+        temperature: 305,
+        humidity: 3.7,
+        wind: 67,
+        chance_rain: 4.3,
+      },
+      {
+        time: "5 am",
+        temperature: 356,
+        humidity: 16.0,
+        wind: 49,
+        chance_rain: 3.9,
+      },
+      {
+        time: "6 am",
+        temperature: 375,
+        humidity: 0.0,
+        wind: 94,
+        chance_rain: 0.0,
+      },
+      {
+        time: "7 am",
+        temperature: 392,
+        humidity: 0.2,
+        wind: 98,
+        chance_rain: 0,
+      },
+      {
+        time: "8 am",
+        temperature: 408,
+        humidity: 3.2,
+        wind: 87,
+        chance_rain: 6.5,
+      },
+      {
+        time: "9 am",
+        temperature: 452,
+        humidity: 25.0,
+        wind: 51,
+        chance_rain: 4.9,
+      },
+      {
+        time: "10 am",
+        temperature: 518,
+        humidity: 26.0,
+        wind: 65,
+        chance_rain: 7,
+      },
+    ],
+    headers: [
+      {
+        text: "Thời gian",
+        align: "start",
+        sortable: false,
+        value: "time",
+      },
+      { text: "Nhiệt độ", value: "temperature" },
+      { text: "Độ ẩm", value: "humidity" },
+      { text: "Tốc độ gió", value: "wind" },
+      { text: "Khả năng mưa", value: "chance_rain" },
+    ],
+  }),
 };
 </script>
 <style scope>
@@ -197,78 +244,384 @@ export default {
   height: 100%;
   overflow: hidden;
 }
-.rain-drop {
-  position: fixed;
-  top: -100px;
-  width: 1px;
-  height: 100px;
-  background-image: linear-gradient(to bottom, transparent, white);
+@import url("https://fonts.googleapis.com/css?family=Raleway");
+@font-face {
+  font-family: 'Raleway';
+  font-style: normal;
+  font-weight: 400;
+  src: url(https://fonts.gstatic.com/s/raleway/v18/1Ptxg8zYS_SKggPN4iEgvnHyvveLxVvaorCFPrEHJA.woff2) format('woff2');
+  unicode-range: U+0460-052F, U+1C80-1C88, U+20B4, U+2DE0-2DFF, U+A640-A69F, U+FE2E-FE2F;
 }
-.rain-drop:nth-child(2) {
-  left: 10%;
-  animation: rainFall 0.8s linear 2s infinite;
+/* .hot {
+  position: absolute;
+  top: 10%;
+  left: 40%;
+  background: linear-gradient(
+    to top right,
+    rgba(255, 87, 34, 1) 0%,
+    rgba(251, 140, 0, 1) 100%
+  );
+  box-shadow: 1px 1px 30px rgba(255, 111, 0, 1);
+} */
+/* .sun {
+  position: absolute;
+  left: 30%;
+  width: 200px;
+  height: 200px;
+  border-radius: 100%;
+  background: linear-gradient(
+    to bottom left,
+    rgba(255, 235, 59, 1) 0%,
+    rgba(249, 168, 37, 1) 90%
+  );
+  box-shadow: 1px 1px 30px rgba(255, 160, 0, 1);
+  animation: inex 3s infinite linear;
 }
-.rain-drop:nth-child(3) {
-  left: 15%;
-  animation: rainFall 0.7s linear 1s infinite;
-}
-.rain-drop:nth-child(4) {
-  left: 35%;
-  animation: rainFall 0.7s linear 0.1s infinite;
-}
-.rain-drop:nth-child(5) {
-  left: 65%;
-  animation: rainFall 0.7s linear 0.3s infinite;
-}
-.rain-drop:nth-child(6) {
-  left: 80%;
-  animation: rainFall 0.8s linear 0.9s infinite;
-}
-.rain-drop:nth-child(7) {
-  left: 90%;
-  animation: rainFall 0.6s linear 1.1s infinite;
-}
-.rain-drop:nth-child(8) {
-  left: 82%;
-  animation: rainFall 0.7s linear 0.4s infinite;
-}
-.rain-drop:nth-child(9) {
-  left: 95%;
-  animation: rainFall 0.7s linear 1s infinite;
-}
-.rain-drop:nth-child(10) {
-  left: 53%;
-  animation: rainFall 0.8s linear 0.5s infinite;
-}
-.rain-drop:nth-child(11) {
+
+.sunx {
+  position: absolute;
+  top: 30%;
   left: 45%;
-  animation: rainFall 0.8s linear 0.5s infinite;
+  width: 10px;
+  height: 10px;
+  border-radius: 100%;
+  background-color: #fff;
+  opacity: 0.2;
 }
-.rain-drop:nth-child(12) {
-  left: 5%;
-  animation: rainFall 0.8s linear 0.2s infinite;
+.cloudy {
+  position: absolute;
+  top: 25%;
+  left: 45%;
+  background: linear-gradient(
+    to top right,
+    rgba(63, 81, 181, 1) 0%,
+    rgba(3, 155, 229, 1) 40%
+  );
+  box-shadow: 1px 1px 30px rgba(2, 119, 189, 1);
 }
-.rain-drop:nth-child(13) {
-  left: 33%;
-  animation: rainFall 0.8s linear 0.65s infinite;
+.cloud {
+  position: absolute;
+  top: 5%;
+  left: 70%;
+  width: 60px;
+  height: 20px;
+  border-radius: 10px;
+  background-color: rgba(129, 212, 250, 1);
+  box-shadow: 1px 1px 30px rgba(0, 151, 167, 1);
+  animation: move 3s infinite linear;
 }
-.rain-drop:nth-child(14) {
-  left: 66%;
-  animation: rainFall 0.7s linear 0.35s infinite;
+.cloudx {
+  position: absolute;
+  top: 23%;
+  left: 55%;
+  width: 60px;
+  height: 20px;
+  border-radius: 10px;
+  background-color: rgba(129, 212, 250, 1);
+  animation: move 3s infinite linear;
 }
-.rain-drop:nth-child(15) {
+.stormy {
+  position: absolute;
+  top: 25%;
+  left: 70%;
+  background: linear-gradient(
+    to top right,
+    rgba(117, 117, 117, 1) 0%,
+    rgba(33, 33, 33, 1) 90%
+  );
+  box-shadow: 1px 1px 30px rgba(33, 33, 33, 1);
+}
+.stormy li {
+  position: absolute;
+  list-style: none;
+  width: 5px;
+  height: 5px;
+  border-radius: 100%;
+  background-color: #777;
+  box-shadow: 1px 1px 30px #555;
+  animation: fall 3s infinite linear;
+  opacity: 0;
+}
+.stormy li:nth-child(1) {
+  top: 0%;
+  left: 100%;
+}
+
+.stormy li:nth-child(2) {
+  top: 5%;
+  left: 90%;
+}
+
+.stormy li:nth-child(3) {
+  top: -10%;
+  left: 80%;
+  animation-delay: 2s;
+}
+
+.stormy li:nth-child(4) {
+  top: 10%;
+  left: 100%;
+  animation-delay: 2s;
+}
+
+.stormy li:nth-child(5) {
+  top: 20%;
+  left: 80%;
+  animation-delay: 0.5s;
+}
+
+.stormy li:nth-child(6) {
+  top: 35%;
+  left: 70%;
+  background-color: #bbb;
+  animation-delay: 0.5s;
+}
+
+.stormy li:nth-child(7) {
+  top: 23%;
+  left: 60%;
+  background-color: #bbb;
+  animation-delay: 0.8s;
+}
+
+.stormy li:nth-child(8) {
+  top: 5%;
+  left: 70%;
+  background-color: #bbb;
+  animation-delay: 0.8s;
+}
+.snowe {
+  position: absolute;
+  top: 60%;
+  left: 40%;
+  width: 40px;
+  height: 40px;
+  border-radius: 60px;
+  background-color: #ddd;
+}
+.snowex {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 15px;
+  height: 15px;
+  border-radius: 15px;
+  background-color: #bbb;
+}
+.stick {
+  position: absolute;
+  top: 60%;
+  left: 40%;
+  width: 3px;
+  height: 15px;
+  transform: rotate(-45deg);
+  background-color: #333;
+  z-index: -10;
+}
+.stick2 {
+  position: absolute;
+  top: 60%;
+  left: 70%;
+  width: 3px;
+  height: 15px;
+  transform: rotate(45deg);
+  background-color: #333;
+  z-index: -10;
+}
+.breezy {
+  position: absolute;
+  top: 60%;
+  left: 30%;
+  background: linear-gradient(
+    to top right,
+    rgba(156, 204, 101, 1) 0%,
+    rgba(38, 198, 218, 1) 50%
+  );
+  box-shadow: 1px 1px 30px rgba(38, 198, 218, 1);
+}
+.cloudr {
+  position: absolute;
+  top: 5%;
+  left: 60%;
+  width: 60px;
+  height: 20px;
+  border-radius: 10px;
+  background-color: rgba(96, 125, 139, 1);
+  box-shadow: 1px 1px 30px rgba(84, 110, 122, 1);
+  animation: flash 1.5s infinite linear;
+}
+
+.breezy li {
+  position: absolute;
+  list-style: none;
+  width: 2px;
+  height: 7px;
+  border-radius: 10%;
+  background-color: #eee;
+  opacity: 0;
+  animation: fall 3s infinite linear;
+  transform: rotate(25deg);
+}
+.breezy li:nth-child(1) {
+  top: 10%;
+  left: 95%;
+}
+
+.breezy li:nth-child(2) {
+  top: 5%;
+  left: 83%;
+  animation-delay: 0.3s;
+}
+
+.breezy li:nth-child(3) {
+  top: 4%;
+  left: 87%;
+  animation-delay: 0.6s;
+}
+
+.breezy li:nth-child(4) {
+  top: 15%;
+  left: 70%;
+  animation-delay: 1s;
+}
+
+.breezy li:nth-child(5) {
+  top: 10%;
+  left: 75%;
+}
+
+.night {
+  position: absolute;
+  top: 60%;
+  left: 60%;
+  background: linear-gradient(
+    to bottom right,
+    rgba(63, 81, 181, 1) 0%,
+    rgba(171, 71, 188, 1) 70%
+  );
+  box-shadow: 1px 1px 30px rgba(81, 45, 168, 1);
+}
+.moon {
+  position: absolute;
+  top: -10%;
+  left: 65%;
+  width: 70px;
+  height: 70px;
+  border-radius: 100%;
+  background: rgba(255, 241, 118, 1);
+  box-shadow: 1px 1px 30px rgba(224, 224, 224, 1);
+}
+.spot1 {
+  position: absolute;
+  top: 0%;
+  left: 85%;
+  width: 10px;
+  height: 10px;
+  border-radius: 100%;
+  background-color: #777;
+}
+.spot2 {
+  position: absolute;
+  top: 30%;
+  left: 75%;
+  width: 5px;
+  height: 5px;
+  border-radius: 100%;
+  background-color: #777;
+}
+
+.night li {
+  position: absolute;
+  list-style: none;
+  width: 3px;
+  height: 3px;
+  border-radius: 100%;
+  background-color: #fff;
+
+  transform: rotate(45deg);
+}
+.night li:nth-child(1) {
+  top: 30%;
+  left: 65%;
+}
+
+.night li:nth-child(2) {
+  top: 35%;
   left: 53%;
-  animation: rainFall 0.8s linear 0.2s infinite;
 }
-@keyframes rainFall {
-  0% {
-    top: -100px;
-  }
+
+.night li:nth-child(3) {
+  opacity: 0;
+  top: 20%;
+  left: 75%;
+  width: 2px;
+  height: 2px;
+  animation: meteor 1.5s infinite linear;
+  animation-delay: 0.6s;
+}
+
+.night li:nth-child(4) {
+  top: 5%;
+  left: 50%;
+}
+
+.night li:nth-child(5) {
+  opacity: 0;
+  top: 20%;
+  left: 55%;
+  width: 1px;
+  height: 15px;
+  animation: meteor 1.5s infinite linear;
+}
+
+@keyframes inex {
   50% {
-    top: 100vh;
+    opacity: 0.4;
+  }
+  60% {
+    opacity: 1;
+  }
+}
+@keyframes move {
+  50% {
+    transform: translateX(-10px);
+  }
+}
+@keyframes fall {
+  10% {
+    opacity: 0.8;
+  }
+
+  50% {
+    opacity: 1;
+    transform: translate(-10px, 30px);
   }
   100% {
-    top: 100vh;
+    transform: translate(-25px, 70px);
   }
 }
+@keyframes flash {
+  48% {
+    background-color: rgba(96, 125, 139, 1);
+  }
+  50% {
+    background-color: #fff;
+  }
+  55% {
+    background-color: rgba(96, 125, 139, 1);
+  }
+  57% {
+    background-color: #fff;
+  }
+}
+@keyframes meteor {
+  10% {
+    opacity: 1;
+  }
+  80% {
+    left: 10%;
+    top: 75%;
+    opacity: 0;
+  }
+}*/
 </style>
